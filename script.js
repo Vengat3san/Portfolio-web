ad41874a7df6154efa83b9c9d78d20b23a2954f5
Vengat3san <vengatesanganapathy001@gmail.com>
2024-12-06 16:24:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -54,3 +54,12 @@ function animate() {
 
 // Start animation
 animate();
+
+document.querySelectorAll('nav ul li a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const targetId = link.getAttribute('href').substring(1);
+    document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
